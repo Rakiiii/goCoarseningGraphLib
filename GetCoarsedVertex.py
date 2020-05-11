95138c364c6a254ae5d2a4addf4a2dcc8779affa
Smurf <sudaru2@gmail.com>
2020-05-11 21:49:59 +0300

    add coarsing with linear regression

---
+++ GetCoarsedVertex.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pickle
+
+clf = pickle.load(open('model.pkl', 'rb'))
+
+vertexPairs = pd.read_csv(r'pairs.csv')
+predictions = clf.predict_proba(vertexPairs.values)
+resFile = open('contractedPairs', 'w')
+for i in range(len(predictions)):
+    if predictions[i][1] >= 0.9:
+        resFile.write("1 ")
+    else:
+        resFile.write("0 ")
+resFile.close()
---
+++ linregRegressionContracter.go
@@ -0,0 +1,116 @@
+package coarseninggraph
+
+import (
+	"bufio"
+	"encoding/csv"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+const (
+	modelPath  = "model.pkl"
+	resultPath = "contractedPairs"
+	dataPath   = "pairs.csv"
+	linergPath = "GetCoarsedVertex.py"
+)
+
+type data struct {
+	Overlap  int
+	Diff     int
+	Incedent bool
+}
+
+var GlobalWriter *csv.Writer
+
+func (d data) GetSlice() []string {
+	res := make([]string, 3)
+	res[0] = strconv.FormatFloat(float64(d.Overlap), 'f', -1, 64)
+	res[1] = strconv.FormatFloat(float64(d.Diff), 'f', -1, 64)
+	if d.Incedent {
+		res[2] = "1.0"
+	} else {
+		res[2] = "0.0"
+	}
+	return res
+}
+
+func createRawResultFile() {
+	f, err := os.Create(dataPath)
+	if err != nil {
+		log.Panic(err)
+	} else {
+		GlobalWriter = csv.NewWriter(f)
+		GlobalWriter.Write([]string{"diff", "overlap", "incedent"})
+	}
+}
+
+func IsIncedent(graph *Graph, fv, sv int) bool {
+	for _, v := range graph.GetEdges(fv) {
+		if v == sv {
+			return true
+		}
+	}
+	return false
+}
+
+func collectRawData(graph *Graph) {
+	d := data{}
+	for fv := 0; fv < graph.AmountOfVertex(); fv++ {
+		for sv := fv + 1; sv < graph.AmountOfVertex(); sv++ {
+			d.Diff = countSliceDiff(graph.GetEdges(fv), graph.GetEdges(sv))
+			d.Overlap = countSliceOverlap(graph.GetEdges(fv), graph.GetEdges(sv))
+			d.Incedent = IsIncedent(graph, fv, sv)
+			GlobalWriter.Write(d.GetSlice())
+		}
+	}
+	GlobalWriter.Flush()
+}
+
+func (g *Graph) GetContractedWithLinRegGraph(n int) (*Graph, [][]int, error) {
+	createRawResultFile()
+	collectRawData(g)
+
+	model := exec.Command("bash", "-c", "python "+linergPath)
+	if err := model.Run(); err != nil {
+		return nil, nil, err
+	}
+
+	set, err := readCoarsing(g.AmountOfVertex())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gr, ord := g.contractVertex(set)
+	return gr, ord, nil
+}
+
+func readCoarsing(av int) ([][]int, error) {
+	file, err := os.Open(resultPath)
+	if err != nil {
+		return nil, err
+	}
+
+	//constract set for contracting vertex
+	result := make([][]int, av)
+	for i, _ := range result {
+		result[i] = make([]int, 1)
+		result[i][0] = i
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	partition := strings.Fields(scanner.Text())
+
+	for i := 0; i < av; i++ {
+		for j := i + 1; j < av; j++ {
+			pos := (j - i - 1) + i*av
+			if partition[pos] == "1" {
+				contractVertex(result, i, j)
+			}
+		}
+	}
+	return result, nil
+}
